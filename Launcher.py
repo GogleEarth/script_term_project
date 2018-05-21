772eb9d6733748fe170d592232846d8e35da3a3b
GogleEarth <32813163+GogleEarth@users.noreply.github.com>
2018-05-22 00:10:53 +0900

    모든 검색기능 구현완료
    모든 검색기능구현을 완료하였음.
    추가적으로 구현하려 한다면 월단위 검색에 필요할듯함.

--- Launcher.py
+++ Launcher.py
@@ -1,23 +1,28 @@
 import realtime_search
 import monthly_search
+import badair_search
 
 service_key = 'fD%2FcMGFxBktwTG9%2BdUNuSZG%2FCnhfGOUAeEXyQUz6woSWm3JNpQazLAdKEmDuuYd7XZAmOnf6kWcWt49MrbnqcQ%3D%3D'
-database = []
 databaseformonth = []
+databaseforstation = []
+databaseforbaddata = []
+
 
 def printmenu():
     print("--------------------------------------------")
     print("1. 실시간 조회")
     print("2. 월별 측정 정보 조회")
-    print("3. 대기상태가 나쁨인 지역 조회")
+    print("3. 통합 대기 상태가 나쁨인 지역 조회")
+    print("4. 프로그램 종료")
     print("--------------------------------------------")
 
 def selectmenu():
-    global service_key,database,databaseformonth,monthly_search_flag
+    global service_key,database,databaseformonth,monthly_search_flag,databaseforstation
 
     num = str(input("메뉴선택 : "))
     if(num == '1'):
-        realtime_search.realtime_search(service_key,database)
+        station_name = str(input('측정소 이름 : '))
+        realtime_search.realtime_search(service_key, station_name)
     elif(num == '2'):
         station_name = str(input('측정소 이름 : '))
         month = str(input("월 입력(최근 3개월까지)(5월이면 05) : "))
@@ -25,7 +30,12 @@ def selectmenu():
         printdatabasemonth(databaseformonth,month,station_name)
         databaseformonth.clear()
     elif(num == '3'):
-        pass
+        badair_search.badair_search(service_key,databaseforstation)
+        for name in databaseforstation:
+            realtime_search.realtime_search(service_key,name)
+    elif(num == '4'):
+        print("프로그램을 종료합니다.")
+        exit(1)
     else:
         print("잘못 된 입력입니다.")
 
--- realtime_search.py
+++ realtime_search.py
@@ -3,12 +3,9 @@ import urllib.request
 import urllib.parse
 from xml.dom.minidom import parseString
 
-def realtime_search(service_key,database):
+def realtime_search(service_key,station_name):
 
-    station_name = str(input('측정소 이름 : '))
-    #city_name = str(input('도시이름 : '))
     url = "http://openapi.airkorea.or.kr/openapi/services/rest/ArpltnInforInqireSvc/getMsrstnAcctoRltmMesureDnsty?serviceKey="+service_key+"&numOfRows=1&pageSize=1&pageNo=1&startPage=1&stationName="+urllib.parse.quote(station_name)+"&dataTerm=DAILY&ver=1.3"
-    #url2 = "http://openapi.airkorea.or.kr/openapi/services/rest/ArpltnInforInqireSvc/getCtprvnRltmMesureDnsty?serviceKey="+service_key+"&numOfRows=10&pageSize=10&pageNo=1&startPage=1&sidoName="+urllib.parse.quote(city_name)+"&ver=1.3"
     req = urllib.request.Request(url)
     resp = urllib.request.urlopen(req)
     rescode = resp.getcode()
@@ -46,6 +43,5 @@ def realtime_search(service_key,database):
 
         newdata = data(station_name,time,so2,co,o3,no2,pm10,pm25)
         newdata.print_data()
-        database.append(newdata)
     else:
         print("에러 코드 : " + str(rescode))
